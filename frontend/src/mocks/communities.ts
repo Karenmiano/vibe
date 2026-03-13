730fc1ca5765e0e45879fb4e653ad4e1ad68c995
Karen Miano <mianokaren1798@gmail.com>
2026-03-13 12:52:11 +0300

    Implement communities UI

---
+++ frontend/src/mocks/communities.ts
@@ -0,0 +1,44 @@
+export const communities = [
+  {
+    topic:
+      "Did the Israel/US - Iran war just accidentally trigger the conditions for a global religious conflict? Or is this just another misdirection?",
+    thumbnail: "/images/us-iran.jpg",
+    leadText:
+      "Things escalated way faster than anyone expected. Are we sleepwalking into something bigger? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
+    followersCount: 100,
+    theme: "#181f1b",
+  },
+  {
+    topic: "So I watched Frankenstein, and I cried",
+    thumbnail: "/images/frankenstein.jpeg",
+    leadText:
+      "I went in expecting horror and came out questioning what it means to be human. Anyone else?",
+    followersCount: 6,
+    theme: "#4a392e",
+  },
+  {
+    topic: "The Extinction of the Brush?",
+    thumbnail: "/images/painting.jpg",
+    leadText:
+      "AI can generate a painting in seconds. So why should anyone still pick up a brush?",
+    followersCount: 664,
+    theme: "#5f4a05",
+  },
+  {
+    topic: "My weekend plans have never changed since 1999",
+    thumbnail: "/images/gaming.jpg",
+    leadText:
+      "Same couch, same controller, same vibes. Some things just don't need an upgrade.",
+    followersCount: 500,
+    theme: "#02265f",
+  },
+  {
+    topic:
+      "Does anyone else miss the good old days where you could say 'I am busy' but actually not be busy?",
+    thumbnail: "/images/busy.jpg",
+    leadText:
+      "Now everyone can see you're online, check your last seen, and know you're lying. RIP peace.",
+    followersCount: "2.5k",
+    theme: "#371c1b",
+  },
+];
--- frontend/src/routes/app/index.tsx
+++ frontend/src/routes/app/index.tsx
@@ -1,3 +1,5 @@
+import { communities } from "@/mocks/communities";
+import { UsersIcon } from "@phosphor-icons/react";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app/")({
@@ -7,9 +9,30 @@ export const Route = createFileRoute("/app/")({
 function RouteComponent() {
   return (
     <div>
-      <section>
-        <h2>Communities</h2>
-        <p>Popular chat rooms</p>
+      <section className="communities">
+        <header>
+          <h2>Communities</h2>
+          <p>Popular chat rooms</p>
+        </header>
+
+        <ul className="communities-list">
+          {communities.map((community) => (
+            <li style={{ backgroundColor: community.theme }}>
+              <div className="community-header">
+                <img src={community.thumbnail} alt="" className="thumbnail" />
+                <div>
+                  <h3 className="topic">{community.topic}</h3>
+                  <div className="followers-count">
+                    <UsersIcon />
+                    <p>{community.followersCount} Followers</p>
+                  </div>
+                </div>
+              </div>
+              <p className="lead-text">{community.leadText}</p>
+              <button className="join-button">Join</button>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
